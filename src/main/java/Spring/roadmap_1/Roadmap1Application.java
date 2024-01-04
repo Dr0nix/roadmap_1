c5ae8fce997156a7fea59c695654d8fdffeed8ef
minjuxpp <minjuxpp@naver.com>
2024-01-04 18:01:19 +0900

    first section

---
+++ src/main/java/Spring/roadmap_1/Roadmap1Application.java
@@ -0,0 +1,13 @@
+package Spring.roadmap_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roadmap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Roadmap1Application.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/roadmap_1/Roadmap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.roadmap_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roadmap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
